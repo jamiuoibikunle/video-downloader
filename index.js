cb063e3444e152e538ed51f20cf4be0b3e01b537
jamiu ibikunle <jamiuoibikunle@gmail.com>
2022-09-24 11:47:58 +0100

    Done with landing page

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const app = require("express")()
+const express = require('express');
+const app = express();
+const path = require('path');
 const youtube = require("./routes/youtube")
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8000
 
+app.use(express.static(path.join(__dirname, "public")))
 app.use("/youtube", youtube)
 
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("pages/index")
+})
+
+app.get("/download", (req, res) => {
+    res.render("pages/download")
+})
+
+
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`)
+    console.log(`Running at http://localhost:${PORT}`)
 })
